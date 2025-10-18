d12fa088e91e92e39a3e739ad2849917e186eb95
Cyril Fischer <github@cyril-fischer.fr>
2025-10-18 15:06:08 +0200

    add: Foudry - Deploy & Script

---
+++ 3. Outils Dev/Foundry/script/SimpleStorage.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import "forge-std/Script.sol";
+import "../src/SimpleStorage.sol";
+
+contract MyScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        
+        SimpleStorage simpleStorage = new SimpleStorage(3);
+
+        vm.stopBroadcast();
+    }
+}
--- 3. Outils Dev/Foundry/src/SimpleStorage.sol
+++ 3. Outils Dev/Foundry/src/SimpleStorage.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-contract SimpleStorage {
-    uint256 private number;
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
+contract SimpleStorage is Ownable {
+    uint256 private number;
+    
+    constructor(uint256 _number) Ownable(msg.sender){
+        number = _number;
+    }
+    
     function setNumber(uint256 _number) external {
         number = _number;
     }
